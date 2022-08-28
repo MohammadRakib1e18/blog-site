e22b5fd5f59cebeb1b28d2928cb9793e01bbb4b1
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-08-29 01:50:49 +0600

    post background changed and transition added

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-/* 
-   navbar configuration
-   (hide, display based on user's mouse scrolling)
- */
-document.getElementById('body').addEventListener('wheel', function(event){
-    
-    if(event.deltaY <=0){
-        document.getElementById('navbar').classList.add('display-nav');
-        document.getElementById('navbar').classList.remove('hide-nav');
-        console.log(event.deltaY);
-    }
-    else{
-        document.getElementById('navbar').classList.add('hide-nav');
-        document.getElementById('navbar').classList.remove('display-nav');
-    }
-})
-document.getElementById('navbar').classList.add('display-nav');
+
+
